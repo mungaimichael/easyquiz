58171270a046d60fca76ca25ccf8cd73d7bcb511
mungaimichael <mungaimikey@gmail.com>
2024-11-04 19:04:34 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar";
+
+export default  function App() {
+    return (
+        <div className="bg-black min-h-screen flex flex-col px-3 ">
+            <Navbar/>
+        </div>
+    )
+}
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,8 @@
+export default  function Navbar() {
+
+    return (
+        <div className={`w-full h-14 bg-amber-50 flex items-center justify-between  `} >
+            <h3 className="font-heading" >Easy <span className="text-primary" >quiz</span> </h3>
+        </div>
+    )
+}
