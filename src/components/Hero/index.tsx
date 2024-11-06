4f2a15bb355a053e4bba4a74c614973f7bf8222f
mungaimichael <mungaimikey@gmail.com>
2024-11-06 15:49:41 +0300

    chore:added hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,11 +1,50 @@
 export default  function Hero () {
     return (
         <div
-        className=" h-full w-full flex flex-col pt-14 "
+        className=" h-full w-full flex flex-col justify-around items-center "
         >
-            <h3 className="font-heading font-medium capitalize text-4xl  text-center ">
-                Fun and easy games to play and enjoy
-            </h3>
+            <div
+                className="h-full w-full flex flex-col  gap-4 items-center"
+            >
+                <h3 className=" text-secondary font-heading font-semibold  capitalize text-4xl  text-center ">
+                    Fun and easy games to play and enjoy
+                </h3>
+                <p className="  lowercase text-md  text-center ">
+                    easy to use and fun to play
+                </p>
+                <div
+                className="w-full flex justify-center gap-4"
+                >
+                    <button className="bg-accent text-text py-3 px-8 rounded-lg font-body">
+                        Get Started
+                    </button>
+                    <button className="bg-secondary text-accent rounded-lg py-3 px-8 font-body ">
+                        Learn More</button>
+                </div>
+                <div
+                    className={"grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 place-items-center"}
+                >
+                    <div
+                        className="w-5/6 h-5/6 rounded-xl flex justify-center items-center bg-secondary"
+                    >
+                        <img
+                            src="/images/1.png"
+                            alt="1"
+                            className="w-5/6 h-full object-contain"
+                        />
+                    </div>
+                    <div
+                        className="w-5/6 h-5/6 rounded-xl flex justify-center items-center bg-accent"
+                    >
+                        <img
+                            src="/images/2.png"
+                            alt="2"
+                            className="w-5/6 h-full object-contain"
+                        />
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
