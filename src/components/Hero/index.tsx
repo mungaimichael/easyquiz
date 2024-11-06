d3bd9d8eea51ae8633ee2f9ee1edc812b57ed484
mungaimichael <mungaimikey@gmail.com>
2024-11-06 13:04:32 +0300

    chore:hero section

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,11 @@
+export default  function Hero () {
+    return (
+        <div
+        className=" h-full w-full flex flex-col pt-14 "
+        >
+            <h3 className="font-heading font-medium capitalize text-4xl  text-center ">
+                Fun and easy games to play and enjoy
+            </h3>
+        </div>
+    )
+}
