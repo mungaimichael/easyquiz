09e7ccbf5f56ba52ed6b9cf16d69a4b204848fe0
mungaimichael <mungaimikey@gmail.com>
2024-11-06 13:04:52 +0300

    chore:Homepage

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,9 @@
+import Hero from "../../components/Hero";
+
+export default  function Homepage() {
+    return (
+        <div className={"w-full h-[94vh] "} >
+            <Hero/>
+        </div>
+    )
+}
