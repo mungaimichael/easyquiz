7ec3100aacfa587dfb66f7d4f622b49903c620a2
mungaimichael <mungaimikey@gmail.com>
2024-11-06 19:19:38 +0300

    feat:added register page and form

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,12 @@
+import Form from "../../components/Form";
+
+export default  function LoginPage() {
+    return (
+        <div className=" w-full h-[91vh]    " >
+            <div className="w-full h-full flex flex-col items-center justify-start" >
+                <h1 className="text-xl font-bold">Register</h1>
+                <Form/>
+            </div>
+        </div>
+    )
+}
