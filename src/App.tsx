b4397ddf179bad457b0f602f7c4bfb4413644bc2
mungaimichael <mungaimikey@gmail.com>
2024-11-06 13:06:07 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar";
+import AppRouter from "./router";
 
 export default  function App() {
     return (
-        <div className="bg-black min-h-screen flex flex-col px-3 ">
-            <Navbar/>
+        <div className="bg-background min-h-screen flex flex-col px-3 ">
+            <AppRouter/>
         </div>
     )
 }
