dd38a9ec140e2c8ff5f347f1a3093089ac57b91c
mungaimichael <mungaimikey@gmail.com>
2024-11-09 20:08:59 +0300

    refactor:fixed types

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+interface Question {
+    id:number,
+    questionTitle:string,
+    questionOptions:string[],
+    difficultyLevel:string,
+    correctAnswer:string,
+}
+
+
+
+export type { Question}
